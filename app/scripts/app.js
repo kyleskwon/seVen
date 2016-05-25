eae069152775522c7947b9cd14da9ce1a65d7237
Kyle Kwon <kyleskwon@gmail.com>
2016-05-25 15:58:12 -0700

    configure firebase, incomplete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('seVen', ["firebase"]);
+
+app.controller("SampleCtrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://seven-9d492.firebaseio.com");
+  // download the data into an array
+  $scope.task = $firebaseArray(ref);
+  // putting a console.log here won't work, see below
+});
