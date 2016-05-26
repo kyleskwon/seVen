6a7d74c3de801553bc6eef2cb0478f1aeb19bec3
Kyle Kwon <kyleskwon@gmail.com>
2016-05-25 19:15:26 -0700

    checkpoint 2 incomplete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,11 @@ var app = angular.module('seVen', ["firebase"]);
 app.controller("SampleCtrl", function($scope, $firebaseArray) {
   var ref = new Firebase("https://seven-9d492.firebaseio.com");
   // download the data into an array
-  $scope.task = $firebaseArray(ref);
+  $scope.messages = $firebaseArray(ref);
   // putting a console.log here won't work, see below
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+        text: $scope.newMessageText
+    });
+  };
 });
