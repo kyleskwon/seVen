ab312682d901e8cd7f735ae5ad68fc79a6c75c3b
Kyle Kwon <kyleskwon@gmail.com>
2016-05-31 10:59:29 -0700

    change messages to tasks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,19 @@
 var app = angular.module('seven', ["firebase"]);
 
-app.controller("SampleCtrl", function($scope, $firebaseObject) {
-  var ref = new Firebase("https://se7en.firebaseio.com/");
-  // download the data into a local object
-  $scope.data = $firebaseObject(ref);
-});
+//app.controller("SampleCtrl", function($scope, $firebaseObject) {
+//  var ref = new Firebase("https://se7en.firebaseio.com/");
+//  // download the data into a local object
+//  $scope.data = $firebaseObject(ref);
+//});
 
 app.controller("ProfileCtrl", ["$scope", $firebaseArray",
     function($scope, $firebaseArray) {
-        var messagesRef = new Firebase("https://se7en.firebaseio.com/messages");
+        var tasksRef = new Firebase("https://se7en.firebaseio.com/tasks");
         
-        $scope.messages = $firebaseArray(messagesRef);
+        $scope.tasks = $firebaseArray(tasksRef);
         
-        var query = messagesRef.orderByChild("timestamp").limitToLast(25);
+        var query = tasksRef.orderByChild("timestamp").limitToLast(25);
         
-        $scope.filteredMessages = $firebaseArray(query);
+        $scope.filteredTasks = $firebaseArray(query);
     }
 ]);
