731588029bdffce5be7af1465918a42fb5bb7d08
Kyle Kwon <kyleskwon@gmail.com>
2016-05-31 10:48:23 -0700

    add ProfileCtrl to controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,5 +4,16 @@ app.controller("SampleCtrl", function($scope, $firebaseObject) {
   var ref = new Firebase("https://se7en.firebaseio.com/");
   // download the data into a local object
   $scope.data = $firebaseObject(ref);
-  // putting a console.log here won't work, see below
 });
+
+app.controller("ProfileCtrl", ["$scope", "$firebaseArray",
+    function($scope, $firebaseArray) {
+        var messagesRef = new Firebase("https://se7en.firebaseio.com/messages");
+        
+        $scope.messages = $firebaseArray(messagesRef);
+        
+        var query = messagesRef.orderByChild("timestamp").limitToLast(25);
+        
+        $scope.filteredMessages = $firebaseArray(query);
+    }
+]);
