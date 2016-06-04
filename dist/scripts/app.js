d59b66fa0aa46f646d06f772ca43a7021e068b47
Kyle Kwon <kyleskwon@gmail.com>
2016-06-03 21:30:21 -0700

    add ability to add and delete tasks

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,20 +1,36 @@
 var app = angular.module('seven', ["firebase"]);
 
-//app.controller("SampleCtrl", function($scope, $firebaseObject) {
-//  var ref = new Firebase("https://se7en.firebaseio.com/");
+app.controller("ProfileCtrl", function($scope, $firebaseArray) {
+  var ref = new Firebase("https://se7en.firebaseio.com/");
+    $scope.tasks = $firebaseArray(ref);
 //  // download the data into a local object
 //  $scope.data = $firebaseObject(ref);
 //});
 
-app.controller("ProfileCtrl", ["$scope", "$firebaseArray",
-    function($scope, $firebaseArray) {
-        var tasksRef = new Firebase("https://se7en.firebaseio.com/tasks");
-        
-        $scope.tasks = $firebaseArray(tasksRef);
-        
-        var query = tasksRef.orderByChild("timestamp").limitToLast(25);
-        
-        $scope.filteredTasks = $firebaseArray(query);
-//        $scope.filteredTasks.$add({text: "h"});
-    }
-]);
+//app.controller("ProfileCtrl", ["$scope", "$firebaseArray",
+//    function($scope, $firebaseArray) {
+//        var tasksRef = new Firebase("https://se7en.firebaseio.com/tasks");
+//        
+//        $scope.tasks = $firebaseArray(tasksRef);
+//        
+//        var query = tasksRef.orderByChild("timestamp").limitToLast(25);
+//        
+//        $scope.filteredTasks = $firebaseArray(query);
+////        $scope.filteredTasks.$add({text: "h"});
+//    }
+
+        $scope.addTask = function() {
+            $scope.tasks.$add({
+              text: $scope.newTaskText
+            });
+          };
+});
+
+//app.controller("SampleCtrl", function($scope, $firebaseObject) {
+//  var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/data");
+//  // download the data into a local object
+//  var syncObject = $firebaseObject(ref);
+//  // synchronize the object with a three-way data binding
+//  // click on `index.html` above to see it used in the DOM!
+//  syncObject.$bindTo($scope, "data");
+//});
