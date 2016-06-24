6ab868e6ac7bbf09e5f1ebc3f6775b4756b2e22c
Kyle Kwon <kyleskwon@gmail.com>
2016-06-24 17:46:56 -0400

    create interval function to change task state to expired

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,71 +25,44 @@ seven.config(['$stateProvider', '$locationProvider', function($stateProvider, $l
         });
 }]);
 
-seven.controller("HomeCtrl", function($scope, $interval, $firebaseArray) {
-    var ref = new Firebase("https://se7en.firebaseio.com/");
-    $scope.tasks = $firebaseArray(ref);
-//    $scope.select = 'Choose one';   // dropdown placeholder
-    $scope.priority = ['High', 'Medium', 'Low'];
+seven.controller("HomeCtrl", ["$scope", "$interval", "$firebaseArray", 
+    function($scope, $interval, $firebaseArray) {
+        var ref = new Firebase("https://se7en.firebaseio.com/");
+        $scope.tasks = $firebaseArray(ref);
+//        $scope.priority = ['high', 'medium', 'low'];
     
-    // loop through the task array
-    // check if expired
-    // if expired, change state to expired
-    // look at firebase doc for methods
-    $interval(function() {
-        console.log("A");
-    }, 2000)
+// loop through the task array
+// check if expired
+// if expired, change state to expired
+// look at firebase doc for methods
+        $interval(function() {
+                for (var i = 0; i < $scope.tasks.length; i++) {
+                    var currentTime = (new Date());
+                    if (1 == 1) {
+                        $scope.tasks[i].state = "active";
+                        $scope.tasks.$save(i);
+                    }
+                }
+        }, 3000)
     
-//  Synchronize collections as arrays
-    $scope.addTask = function() {
-        $scope.tasks.$add({
-            description: $scope.task,
-//            priority: ,   // user's choice from $scope.priority
-            state: "active",
-            timeCreated: (new Date()).getTime()
-        }).then(function(ref) {
-            $scope.task = "";
-        });
-    }
+        $scope.addTask = function() {
+            $scope.tasks.$add({
+                description: $scope.task,
+    //            priority: ,   // user's choice from $scope.priority
+                state: "active",
+                timeCreated: (new Date()).getTime()
+            }).then(function(ref) {
+                $scope.task = "";
+            });
+        }
     
-    //$scope.hiding = true;
-    $scope.hideTask = function() {
-        //  hide task after 0.5 sec (500); change to 7 days (604800000)
-        //  var currentTime = new Date().getTime();
-        //  if (currentTime - timeCreated >= 604800000) {
-        //      hide task
-        //  }
-        // function that finds the current time and compares it to the timeCreated + timeLimit
-        // run this check every 5 seconds
-        
-        // set up an interval that 
-        
-        $scope.hiding = false;    
-        $timeout(function() {
-            $scope.state = "complete";
-            $scope.hiding = true; 
-        }, 500);
-    }
-});
 
-//authentication
-//  app.controller("ProfileCtrl", function($scope, $firebaseAuth) {
-//    var ref = new Firebase("https://se7en.firebaseio.com");
-//    
-//    var auth = $firebaseAuth(ref);
-//    
-//    auth.$authWithOAuthPopup("facebook").then(function(authData) {
-//        console.log("Logged in as:", authData.uid);
-//    }).catch(function(error) {
-//        console.log("Authentication failed:", error);
-//    });
-//  });
+    }
+]);
 
-//three-way data binding (notice the $bindTo method)
-//  app.controller("SampleCtrl", function($scope, $firebaseObject) {
-//      var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/data");
-//download the data into a local object
-//      var syncObject = $firebaseObject(ref);
-//synchronize the object with a three-way data binding
-//click on `index.html` above to see it used in the DOM!
-//  syncObject.$bindTo($scope, "data");
-//});
+//  hide task after 0.5 sec (500); change to 7 days (604800000)
+//  var currentTime = new Date().getTime();
+//  if (currentTime - timeCreated >= 604800000) {
+//      hide task
+//  }
+// function that finds the current time and compares it to the timeCreated + timeLimit
