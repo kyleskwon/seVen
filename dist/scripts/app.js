9b0dd4714378955092dd464c1688b2c9eac07380
Kyle Kwon <kyleskwon@gmail.com>
2016-06-24 23:59:34 -0400

    destroy complete and expired states due to error

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -13,54 +13,35 @@ seven.config(['$stateProvider', '$locationProvider', function($stateProvider, $l
             controller: 'HomeCtrl',
             templateUrl: '/templates/home.html'
         });
-//        .state('complete', {
-//            url: '/complete',
-//            controller: 'CompleteCtrl',
-//            templateUrl: '/templates/complete.html'
-//        })
-//        .state('expired', {
-//            url: '/expired',
-//            controller: 'ExpiredCtrl',
-//            templateUrl: '/templates/expired.html'
-//        });
 }]);
 
-seven.controller("HomeCtrl", ["$scope", "$interval", "$firebaseArray", 
-    function($scope, $interval, $firebaseArray) {
-        var ref = new Firebase("https://se7en.firebaseio.com/");
-        $scope.tasks = $firebaseArray(ref);
-//        $scope.priority = ['high', 'medium', 'low'];
+seven.controller("HomeCtrl", function($scope, $interval, $firebaseArray) {
+    var ref = new Firebase("https://se7en.firebaseio.com/");
+    $scope.tasks = $firebaseArray(ref);
+    $scope.priority = ['high', 'medium', 'low'];
     
 // loop through the task array
 // check if expired
 // if expired, change state to expired
 // look at firebase doc for methods
-        $interval(function() {
-            for (var i = 0; i < $scope.tasks.length; i++) {
-                var currentTime = (new Date()).getTime();
-                if (currentTime >= $scope.tasks[i].timeCreated + 3000) {
-                    $scope.tasks[i].state = "expired";
-                    $scope.tasks.$save(i);
-                }
+    $interval(function() {
+        for (var i = 0; i < $scope.tasks.length; i++) {
+            var currentTime = (new Date()).getTime();
+            if (currentTime >= $scope.tasks[i].timeCreated + 3000) {
+                $scope.tasks[i].state = "expired";
+                $scope.tasks.$save(i);
             }
-        }, 3000)
-    
-        $scope.addTask = function() {
-            $scope.tasks.$add({
-                description: $scope.task,
-    //            priority: ,   // user's choice from $scope.priority
-                state: "active",
-                timeCreated: (new Date()).getTime()
-            }).then(function(ref) {
-                $scope.task = "";
-            });
         }
+    }, 3000)
+    
+    $scope.addTask = function() {
+        $scope.tasks.$add({
+            description: $scope.task,
+//            priority: ,   // user's choice from $scope.priority
+            state: "active",
+            timeCreated: (new Date()).getTime()
+        }).then(function(ref) {
+            $scope.task = "";
+        });
     }
-]);
-
-//  hide task after 0.5 sec (500); change to 7 days (604800000)
-//  var currentTime = new Date().getTime();
-//  if (currentTime - timeCreated >= 604800000) {
-//      hide task
-//  }
-// function that finds the current time and compares it to the timeCreated + timeLimit
+});
